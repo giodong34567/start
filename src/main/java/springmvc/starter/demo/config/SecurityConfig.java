435ef92f4f7588cd1a3ed2dc6049f269c37974c0
giodong34567 <pacman16012004@gmail.com>
2024-09-18 10:43:35 +0700

    ok

--- src/main/java/springmvc/starter/demo/config/SecurityConfig.java
+++ src/main/java/springmvc/starter/demo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springmvc.starter.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+@EnableWebSecurity // them dong nay
 @Configuration
 public class SecurityConfig {
 
